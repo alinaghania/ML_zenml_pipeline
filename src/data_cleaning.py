ced8c9afb75819dfd734b75a08752c761c27afa3
Alina Ghani <alina@MacBook-Air-de-Alina.local>
2024-02-06 11:22:43 +0100

    data update

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -4,6 +4,8 @@ from typing import Union
 import numpy as np 
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+
 
 
 class DataStrategy(ABC):
@@ -23,10 +25,12 @@ class CleanData(DataStrategy):
         Preprocess data
         """
         try:
-            data = data.drop(
-                [
-
-                ],
-                axis=1 
-            )
+            
             data = data.dropna() # Drop missing values
+            encoder = OneHotEncoder(sparse=False)
+            X_encoded = encoder.fit_transform(data[['MONTH']])
+            X_encoded = pd.DataFrame(X_encoded, columns=encoder.get_feature_names_out(['MONTH']))
+
+            
+            X_encoded = pd.concat((X_encoded, data.drop(columns=['MONTH'])), axis=1)
+            X_encoded = X_encoded.drop(columns = 'BASEL_temp_min')
