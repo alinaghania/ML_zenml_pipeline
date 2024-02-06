001ac07b413eb465f4a3b64307c0a157533d03e2
benjamincrt <azertyben2000@orange.fr>
2024-02-06 15:54:07 +0100

    [ADD] data cleaning test

--- test/test_data_transformation.py
+++ test/test_data_transformation.py
@@ -1,12 +1,12 @@
 import pytest
 import pandas as pd
-from src.data_cleaning import CleanData, SplitData
+from ..src.data_cleaning import DataPreProcessStrategy, DataDivideStrategy
 
 
-class DataTransformationTest:
-    def clean_data(self):
+class TestDataTransformation():
+    def test_clean_data(self):
         # Init testing variable
-        clean_data = CleanData()
+        clean_data = DataPreProcessStrategy()
         df_test = pd.DataFrame(data=[
                 [19,"female",27.9,0,"yes","southwest",16884.924],
                 [18,"male",33.77,1,"no","southeast",1725.5523],
@@ -14,9 +14,11 @@ class DataTransformationTest:
             ],
             columns=["age","sex","bmi","children","smoker","region","charges"])
         
-        assert clean_data.handle_data(df_test) == pd.DataFrame(data=[
-                [19,27.9,"1",16884.924],
-                [18,33.77,"0",1725.5523],
-                [28,33,"0",4449.462]
+        df_test = clean_data.handle_data(df_test)
+        
+        assert df_test.equals(pd.DataFrame(data=[
+                [19,27.9,1,16884.924],
+                [18,33.77,0,1725.5523],
+                [28,33,0,4449.462]
             ],
-            columns=["age","bmi","smoker","charges"])
+            columns=["age","bmi","smoker","charges"]))
