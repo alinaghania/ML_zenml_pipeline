9d8501ec1976c01142ad9c133864bccf4c4ad175
Alina Ghani <alina@MacBook-Air-de-Alina.local>
2024-02-06 12:34:44 +0100

    push

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -7,13 +7,12 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 
 
-
 class DataStrategy(ABC):
     """
     Abstract class for handling data
     """
     @abstractmethod
-    def handle_data(self, data: pd.DataFrame) -> Union[pd.DataFrame,pd.Series]:
+    def handle_data(self, data: pd.DataFrame) -> Union[pd.DataFrame, pd.Series]:
         pass
 
 class CleanData(DataStrategy):
@@ -25,33 +24,39 @@ class CleanData(DataStrategy):
         Preprocess data
         """
         try:
-            encod = LabelEncoder()
+            encoder = LabelEncoder()
 
-            # Encoding label column 
-        
-            data['sex'] = encod.fit_transform(data['sex'])
-            data['smoker'] = encod.fit_transform(data['smoker'])
+            # Encoding 'sex' and 'smoker' columns
+            data['sex'] = encoder.fit_transform(data['sex'])
+            data['smoker'] = encoder.fit_transform(data['smoker'])
             
-            # One hot encoding for region column
+            # One hot encoding for 'region' column
+            data = pd.get_dummies(data, columns=['region'], prefix='region')
 
-            data = pd.get_dummies(data,columns=['region'],prefix='region')
-            data['region_northeast'] = data['region_northeast'].astype(int)
-            data['region_northwest'] = data['region_northwest'].astype(int)
-            data['region_southeast'] = data['region_southeast'].astype(int)
-            data['region_southwest'] = data['region_southwest'].astype(int)
-
-            # Dropping less correlated columns ( cf matrix correlation)
-            y = data['charges'] # target column
-            x = data.drop(columns=['charges','region_northwest','region_southwest','region_northeast','region_southeast','sex','children'])
-                        
-
-
-            X_train, X_test, y_train, y_test = train_test_split(
-
-                x,
-                y, 
-                test_size=0.2,
-                random_state=0
-
+            # Dropping less correlated columns based on the correlation matrix
+            data = data.drop(
+                ['region_southeast', 'region_southwest', 'region_northeast', 'region_northwest', 'children', 'sex'],
+                axis=1
             )
-            
+
+            return data
+        except Exception as e:
+            logging.error(f"Error in cleaning data: {e}")
+            raise e
+        
+class SplitData(DataStrategy):
+    """
+    Split the data into train and test sets
+    """
+    def handle_data(self, data: pd.DataFrame) -> Union[tuple, None]:
+        """
+        Split the data into training and test sets and return them.
+        """
+        try:
+            y = data['charges']  # Target column
+            X = data[['smoker', 'age', 'bmi']]  # Features
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+            return X_train, X_test, y_train, y_test
+        except Exception as e:
+            logging.error(f"Error in splitting data: {e}")
+            raise e
