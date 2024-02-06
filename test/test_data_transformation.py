3fd5ed622392f9661d4ac9900c84473474328c1c
benjamincrt <azertyben2000@orange.fr>
2024-02-06 18:02:04 +0100

    [ADD] test_split_data

--- test/test_data_transformation.py
+++ test/test_data_transformation.py
@@ -22,3 +22,36 @@ class TestDataTransformation():
                 [28,33,0,4449.462]
             ],
             columns=["age","bmi","smoker","charges"]))
+        
+    def test_split_data(self):
+        # Init testing variable
+        split_data = DataDivideStrategy()
+        df_test = pd.DataFrame(data=[
+                [19,27.90,1,16884.9240],
+                [18,33.77,0,1725.5523],
+                [28,33.00,0,4449.4620]
+            ],
+            columns=["age",	"bmi", "smoker", "charges"])
+
+        X_train, X_test, y_train, y_test = split_data.handle_data(df_test)
+
+        assert X_train.equals(pd.DataFrame(data=[
+                [0,	18,	33.77],
+                [0,	28,	33.00]
+            ],
+            columns=["smoker","age","bmi"],
+            index=[1, 2]))
+
+        assert y_train.equals(pd.Series(
+                data=[1725.5523, 4449.4620],
+                index=[1, 2]
+            ))
+        
+        assert X_test.equals(pd.DataFrame(data=[
+                [1,	19,	27.9]
+            ],
+            columns=["smoker","age","bmi"]))
+
+        assert y_test.equals(pd.Series(
+                [16884.924]
+        ))
